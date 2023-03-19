147ebb044d2b3d0243d3961e5c4c775212f034fb
Darth Vve <osheviremajoroh@gmail.com>
2023-03-19 17:13:55 +0100

    Version 0.7: Partial Dashboard Responsiveness

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,9 +2,13 @@ import './dashboard.scss';
 import { useState, useRef } from 'react';
 import { Navbar } from '../../components';
 import { Sidebar, Users, UserDetails } from '../../containers';
+import { BsWindow, BsWindowSidebar } from 'react-icons/Bs'
 
 const Dashboard = () => {
     const [showUserDetails, setShowUserDetails] = useState(false);
+    const sidebarRef = useRef<HTMLElement | null>(null);
+    const openSidebarEl = document.getElementsByClassName('open-sidebar')[0] as HTMLElement;
+    const closeSidebarEl = document.getElementsByClassName('close-sidebar')[0] as HTMLElement;
 
     const handleShowUserDetails = () => {
         setShowUserDetails(true);
@@ -14,11 +18,30 @@ const Dashboard = () => {
         setShowUserDetails(false);
     };
 
+    
+    const handleOpenSidebar = () => {
+        sidebarRef.current!.style.display = 'block';
+        openSidebarEl.style.display = 'none';
+        closeSidebarEl.style.display = 'block';
+    };
+
+    const handleCloseSidebar = () => {
+        sidebarRef.current!.style.display = 'none';
+        openSidebarEl.style.display = 'block';
+        closeSidebarEl.style.display = 'none';
+    };
+
     return (
         <div className='dashboard'>
             <Navbar />
             <div className='dashboard-content'>
-                <Sidebar onClick={handleShowUsers}/>
+                <div className='open-sidebar' onClick={handleOpenSidebar}>
+                    <BsWindowSidebar size={25}/>
+                </div>
+                <Sidebar onClick={handleShowUsers} ref={sidebarRef}/>
+                <div className='close-sidebar' onClick={handleCloseSidebar}>
+                    <BsWindow size={25}/>
+                </div>
                 {showUserDetails ? (
                     <UserDetails onClick={handleShowUsers} />
                 ) : (
